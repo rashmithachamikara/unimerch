d5f1f62dd80d4581085c6a78e75b75ca1ad553e6
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-12-27 04:23:38 +0530

    Improved footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,9 @@ import styles from './Footer.module.css';
 function Footer() {
     return (
       <footer className={styles.footer}>
+        <br />
         <p>&copy; 2024 UniMerch. All rights reserved.</p>
+        <br />
       </footer>
     );
   }
